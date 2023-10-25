6812910bb928c0dece11c9b471911bb0aea8e8c1
Connor White <connorwhite@ufl.edu>
2023-10-25 15:56:21 -0400

    Check this out yo

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+user_input = int(input('Please enter an option: '))
+
+
+if user_input == 1:
+    password1 = int(input('Please enter your password to encode: '))
+
+    for char in password1:
+
+
+
+elif user_input == 2:
+    pass
+
+else:
+    pass
